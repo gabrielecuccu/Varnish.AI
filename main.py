872a4cb96a124ea017a3054fa9db924f2d6d5db4
gabriele <gabriele.cuccu@gmail.com>
2025-08-14 21:09:04 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,119 @@
+import os
+import sys
+from PySide6 import QtCore, QtWidgets, QtGui
+from dotenv import load_dotenv
+
+from openai import OpenAI
+
+load_dotenv()
+openaiApiKey = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key = openaiApiKey)
+
+class Worker(QtCore.QObject):
+    finished = QtCore.Signal(object)
+    
+    def __init__(self, prompt, userMessage, parent=None):
+        super().__init__(parent)
+        self.prompt = prompt
+        self.userMessage = userMessage
+
+    def run(self):
+        response = client.responses.create(
+            model="gpt-5",
+            reasoning={"effort": "low"},
+            instructions=self.prompt,
+            input=self.userMessage,
+        )
+        
+        self.finished.emit(response.output_text)
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addLayout(self.initHeaderLayout(), 0)
+        layout.addLayout(self.initCentralLayout(), 1)
+        layout.addLayout(self.initFooterLayout(), 0)
+        
+        self.setLayout(layout)
+        
+    def initHeaderLayout(self):
+        iamLayout, self.iamCombo = self.initComboBoxLayout("I am a:", ["developer", "person"])
+        msgTypeLayout, self.msgTypeCombo = self.initComboBoxLayout("Polish my:", ["email", "slack message", "text message"])
+        toneLayout, self.toneCombo = self.initComboBoxLayout("Keeping the tone:", ["professional", "technical", "polite", "ironic"])
+        
+        headerLayout = QtWidgets.QHBoxLayout()
+        headerLayout.addLayout(iamLayout)
+        headerLayout.addLayout(msgTypeLayout)
+        headerLayout.addLayout(toneLayout)
+        
+        return headerLayout
+    
+    def initCentralLayout(self): 
+        self.inputTextEdit = QtWidgets.QPlainTextEdit()
+        self.outputTextEdit = QtWidgets.QPlainTextEdit()
+        self.outputTextEdit.setReadOnly(True)
+        textLayout = QtWidgets.QHBoxLayout()
+        textLayout.addWidget(self.inputTextEdit)
+        textLayout.addWidget(self.outputTextEdit)
+        
+        return textLayout
+    
+    def initFooterLayout(self):
+        footerLayout = QtWidgets.QHBoxLayout()
+        self.progressBar = QtWidgets.QProgressBar()
+        self.goButton = QtWidgets.QPushButton("Go")
+        self.goButton.clicked.connect(self.goClicked)
+        footerLayout.addWidget(self.progressBar, 1)
+        footerLayout.addWidget(self.goButton, 0)
+        
+        return footerLayout
+    
+    def initComboBoxLayout(self, text, items):
+        combo = QtWidgets.QComboBox()
+        combo.addItems(items)
+        
+        label = QtWidgets.QLabel(text)
+        label.setBuddy(combo)
+        
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(combo)
+        
+        return layout, combo
+    
+    @QtCore.Slot()
+    def goClicked(self):
+        iam = self.iamCombo.currentText()
+        msgType = self.msgTypeCombo.currentText()
+        tone = self.toneCombo.currentText()
+        userMessage = self.inputTextEdit.toPlainText()
+        prompt = f"I am a {iam}, polish the following {msgType} keeping the tone {tone}"
+        
+        self.goButton.setEnabled(False)
+        self.progressBar.setRange(0, 0)
+        
+        self.thread = QtCore.QThread()
+        self.worker = Worker(prompt, userMessage)
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.task_finished)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.thread.start()
+        
+    def task_finished(self, result):
+        self.outputTextEdit.setPlainText(result)
+        self.goButton.setEnabled(True)
+        self.progressBar.setRange(0, 100)       
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
