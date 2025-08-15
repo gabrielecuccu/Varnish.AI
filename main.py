41426faaa5815738ad0e4b25aafd4d71d3e0d4f6
gabriele <gabriele.cuccu@gmail.com>
2025-08-15 14:14:24 +0100

    added hint in textbox

--- main.py
+++ main.py
@@ -27,6 +27,21 @@ class Worker(QtCore.QObject):
         
         self.finished.emit(response.output_text)
 
+class HintTextEdit(QtWidgets.QPlainTextEdit):
+    def __init__(self, hint_text="", parent=None):
+        super().__init__(parent)
+        self.hint_text = hint_text
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        if self.toPlainText() == "":
+            painter = QtGui.QPainter(self.viewport())
+            painter.setPen(QtGui.QColor("gray"))
+            painter.setFont(self.font())
+            margin = self.document().documentMargin()
+            painter.drawText(self.viewport().rect().adjusted(int(margin), int(margin), 0, 0),
+                             QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft, self.hint_text)
+
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
@@ -53,7 +68,7 @@ class MyWidget(QtWidgets.QWidget):
         return headerLayout
     
     def initCentralLayout(self): 
-        self.inputTextEdit = QtWidgets.QPlainTextEdit()
+        self.inputTextEdit = HintTextEdit("Write something here")
         self.outputTextEdit = QtWidgets.QPlainTextEdit()
         self.outputTextEdit.setReadOnly(True)
         textLayout = QtWidgets.QHBoxLayout()
